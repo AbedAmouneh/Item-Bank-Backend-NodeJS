323cc3e1ca5ee6c4f6fd9f2b2577926e1d16d917
Abed El-Fattah Amouneh <amounehabed@gmail.com>
2026-03-19 21:58:40 +0200

    Register DELETE /tags/:id route

--- controllers/tagsController/index.ts
+++ controllers/tagsController/index.ts
@@ -1,8 +1,10 @@
 import { HttpWrapper } from '../../platform/http';
+import { deleteTag } from './handlers/delete_tag';
 import { getTags } from './handlers/get_tags';
 import { createTag } from './handlers/post_tag';
 
 export async function tagRoutes(http: HttpWrapper): Promise<void> {
   await http.get('/tags', getTags);
   await http.post('/tags', createTag);
+  await http.delete('/tags/:id', deleteTag);
 }
