8eb29136be8b3c966dae48c04f9492db29f0381b
Abed El-Fattah Amouneh <amounehabed@gmail.com>
2026-03-19 21:35:00 +0200

    Add tags controller index

---
+++ controllers/tagsController/index.ts
@@ -0,0 +1,8 @@
+import { HttpWrapper } from '../../platform/http';
+import { getTags } from './handlers/get_tags';
+import { createTag } from './handlers/post_tag';
+
+export async function tagRoutes(http: HttpWrapper): Promise<void> {
+  await http.get('/tags', getTags);
+  await http.post('/tags', createTag);
+}
