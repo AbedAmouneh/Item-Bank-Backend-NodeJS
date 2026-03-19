481b01f8de34b509d026652638de8b1c4a7798ff
Abed El-Fattah Amouneh <amounehabed@gmail.com>
2026-03-19 18:31:45 +0200

    Add questions controller index and route registration

---
+++ controllers/questionsController/index.ts
@@ -0,0 +1,20 @@
+import { HttpWrapper } from '../../platform/http';
+import { deleteQuestion } from './handlers/delete_question';
+import { getQuestion } from './handlers/get_question';
+import { getQuestions } from './handlers/get_questions';
+import { createQuestion } from './handlers/post_question';
+import { publishQuestion } from './handlers/post_publish';
+import { rejectQuestion } from './handlers/post_reject';
+import { submitForReview } from './handlers/post_submit_for_review';
+import { updateQuestion } from './handlers/put_question';
+
+export async function questionRoutes(http: HttpWrapper): Promise<void> {
+  await http.get('/questions', getQuestions);
+  await http.get('/questions/:id', getQuestion);
+  await http.post('/questions', createQuestion);
+  await http.put('/questions/:id', updateQuestion);
+  await http.delete('/questions/:id', deleteQuestion);
+  await http.post('/questions/:id/submit', submitForReview);
+  await http.post('/questions/:id/publish', publishQuestion);
+  await http.post('/questions/:id/reject', rejectQuestion);
+}
