9ba70cc16d9a02aed39fd985d3b9f401689ee204
Abed El-Fattah Amouneh <amounehabed@gmail.com>
2026-03-23 03:31:49 +0200

    Register GET /game-sessions/stats/me route

--- controllers/gameSessionsController/index.ts
+++ controllers/gameSessionsController/index.ts
@@ -1,10 +1,12 @@
 import { HttpWrapper } from '../../platform/http';
 import { getGameSessions } from './handlers/get_game_sessions';
 import { getLeaderboard } from './handlers/get_leaderboard';
+import { getMyStats } from './handlers/get_my_stats';
 import { createGameSession } from './handlers/post_game_session';
 
 export async function gameSessionRoutes(http: HttpWrapper): Promise<void> {
   await http.post('/game-sessions', createGameSession);
   await http.get('/game-sessions', getGameSessions);
+  await http.get('/game-sessions/stats/me', getMyStats);
   await http.get('/leaderboard', getLeaderboard, true);
 }
