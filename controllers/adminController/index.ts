c1d573a5dba438d94227cd0cd6414255e57a6f21
Abed El-Fattah Amouneh <amounehabed@gmail.com>
2026-03-24 12:18:26 +0200

    Fix POST item-bank route to include :itemBankId path param

--- controllers/adminController/index.ts
+++ controllers/adminController/index.ts
@@ -1,9 +1,12 @@
 import { HttpWrapper } from '../../platform/http';
+import { revokeUserItemBank } from './handlers/delete_user_item_bank';
 import { getUser } from './handlers/get_user';
 import { getUsers } from './handlers/get_users';
+import { getUserItemBanks } from './handlers/get_user_item_banks';
 import { activateUser } from './handlers/post_activate_user';
 import { deactivateUser } from './handlers/post_deactivate_user';
 import { createUser } from './handlers/post_user';
+import { assignUserItemBank } from './handlers/post_user_item_bank';
 import { updateUser } from './handlers/put_user';
 
 export async function adminRoutes(http: HttpWrapper): Promise<void> {
@@ -13,4 +16,7 @@ export async function adminRoutes(http: HttpWrapper): Promise<void> {
   await http.put('/admin/users/:id', updateUser);
   await http.post('/admin/users/:id/activate', activateUser);
   await http.post('/admin/users/:id/deactivate', deactivateUser);
+  await http.get('/admin/users/:id/item-banks', getUserItemBanks);
+  await http.post('/admin/users/:id/item-banks/:itemBankId', assignUserItemBank);
+  await http.delete('/admin/users/:id/item-banks/:itemBankId', revokeUserItemBank);
 }
