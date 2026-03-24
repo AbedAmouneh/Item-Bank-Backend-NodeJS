877e386a1cea67ba5137321bae8f148061cb2328
Abed El-Fattah Amouneh <amounehabed@gmail.com>
2026-03-24 22:03:45 +0200

    Pass roles and tenant_id to softDelete in delete_item_bank handler

--- controllers/itemBanksController/handlers/delete_item_bank.ts
+++ controllers/itemBanksController/handlers/delete_item_bank.ts
@@ -21,7 +21,7 @@ export async function deleteItemBank(
       });
     }
 
-    await service.softDelete(id, request.user.id, request.user.role);
+    await service.softDelete(id, request.user.id, request.user.roles, request.user.tenant_id);
 
     return reply.status(204).send();
   } catch (error) {
